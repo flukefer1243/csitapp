9d70974544af51b9e1321a473d3c593cf432d655
Phanthawat Phiphatkamtorn <flukefer1243@gmail.com>
2022-02-21 14:37:05 +0700

    tekjnolijo

--- csit_mini_project/settings.py
+++ csit_mini_project/settings.py
@@ -14,6 +14,8 @@ from ntpath import join
 from pathlib import Path
 import os
 from tkinter.tix import Tree
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +137,4 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
