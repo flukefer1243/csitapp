7ad4574bbf71acfab885f2f6f2d5b49896c3f961
Phanthawat Phiphatkamtorn <flukefer1243@gmail.com>
2022-02-21 14:41:47 +0700

    first commit

--- csit_mini_project/settings.py
+++ csit_mini_project/settings.py
@@ -137,4 +137,3 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
