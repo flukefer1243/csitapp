5225c997e41959b194f5743afd862d73b7b4847f
Phanthawat Phiphatkamtorn <flukefer1243@gmail.com>
2022-03-16 11:37:13 +0700

    make it better

--- csitweb/views.py
+++ csitweb/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from csitweb import scholar, data, urls
+from csitweb import *
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -8,49 +8,49 @@ from csitweb import scholar, data, urls
 def index(request):
     name = request.GET['name']
     if name == 'wansuree':
-        status = scholar.getData()
+        status = getData()
         # print(status)
-        return render(request, "fontend/index.html", {'info': data.wansuree[0], 'education': data.wansuree[1], 'status':status})
+        return render(request, "fontend/index.html", {'info': wansuree[0], 'education': wansuree[1], 'status':status})
     elif name == 'prasart':
-        return render(request, "fontend/index.html", {'info': data.prasart[0], 'education': data.prasart[1]})
+        return render(request, "fontend/index.html", {'info': prasart[0], 'education': prasart[1]})
     elif name == 'chakkrit':
-        return render(request, "fontend/index.html", {'info': data.chakkrit[0], 'education': data.chakkrit[1]})
+        return render(request, "fontend/index.html", {'info': chakkrit[0], 'education': chakkrit[1]})
     elif name == 'ekkasit':
-        return render(request, "fontend/index.html", {'info': data.ekkasit[0], 'education': data.ekkasit[1]})
+        return render(request, "fontend/index.html", {'info': ekkasit[0], 'education': ekkasit[1]})
     elif name == 'nattavadee':
-        return render(request, "fontend/index.html", {'info': data.nattavadee[0], 'education': data.nattavadee[1]})
+        return render(request, "fontend/index.html", {'info': nattavadee[0], 'education': nattavadee[1]})
     elif name == 'wuttipong':
-        return render(request, "fontend/index.html", {'info': data.wuttipong[0], 'education': data.wuttipong[1]})
+        return render(request, "fontend/index.html", {'info': wuttipong[0], 'education': wuttipong[1]})
     elif name == 'janjira':
-        return render(request, "fontend/index.html", {'info': data.janjira[0], 'education': data.janjira[1]})
+        return render(request, "fontend/index.html", {'info': janjira[0], 'education': janjira[1]})
     elif name == 'adirek':
-        return render(request, "fontend/index.html", {'info': data.adirek[0], 'education': data.adirek[1]})
+        return render(request, "fontend/index.html", {'info': adirek[0], 'education': adirek[1]})
     elif name == 'anongporn':
-        return render(request, "fontend/index.html", {'info': data.anongporn[0], 'education': data.anongporn[1]})
+        return render(request, "fontend/index.html", {'info': anongporn[0], 'education': anongporn[1]})
     elif name == 'tawin':
-        return render(request, "fontend/index.html", {'info': data.tawin[0], 'education': data.tawin[1]})
+        return render(request, "fontend/index.html", {'info': tawin[0], 'education': tawin[1]})
     elif name == 'kreangsak':
-        return render(request, "fontend/index.html", {'info': data.kreangsak[0], 'education': data.kreangsak[1]})
+        return render(request, "fontend/index.html", {'info': kreangsak[0], 'education': kreangsak[1]})
     elif name == 'jaratsri':
-        return render(request, "fontend/index.html", {'info': data.jaratsri[0], 'education': data.jaratsri[1]})
+        return render(request, "fontend/index.html", {'info': jaratsri[0], 'education': jaratsri[1]})
     elif name == 'winai':
-        return render(request, "fontend/index.html", {'info': data.winai[0], 'education': data.winai[1]})
+        return render(request, "fontend/index.html", {'info': winai[0], 'education': winai[1]})
     elif name == 'kraisak':
-        return render(request, "fontend/index.html", {'info': data.kraisak[0], 'education': data.kraisak[1]})
+        return render(request, "fontend/index.html", {'info': kraisak[0], 'education': kraisak[1]})
     elif name == 'sanya':
-        return render(request, "fontend/index.html", {'info': data.sanya[0], 'education': data.sanya[1]})
+        return render(request, "fontend/index.html", {'info': sanya[0], 'education': sanya[1]})
     elif name == 'sutasinee':
-        return render(request, "fontend/index.html", {'info': data.sutasinee[0], 'education': data.sutasinee[1]})
+        return render(request, "fontend/index.html", {'info': sutasinee[0], 'education': sutasinee[1]})
     elif name == 'phisetphong':
-        return render(request, "fontend/index.html", {'info': data.phisetphong[0], 'education': data.phisetphong[1]})
+        return render(request, "fontend/index.html", {'info': phisetphong[0], 'education': phisetphong[1]})
     elif name == 'duangduen':
-        return render(request, "fontend/index.html", {'info': data.duangduen[0], 'education': data.duangduen[1]})
+        return render(request, "fontend/index.html", {'info': duangduen[0], 'education': duangduen[1]})
     elif name == 'thanathorn':
-        return render(request, "fontend/index.html", {'info': data.thanathorn[0], 'education': data.thanathorn[1]})
+        return render(request, "fontend/index.html", {'info': thanathorn[0], 'education': thanathorn[1]})
     elif name == 'nattapon':
-        return render(request, "fontend/index.html", {'info': data.nattapon[0], 'education': data.nattapon[1]})
+        return render(request, "fontend/index.html", {'info': nattapon[0], 'education': nattapon[1]})
     elif name == 'antony':
-        return render(request, "fontend/index.html", {'info': data.antony[0], 'education': data.antony[1]})
+        return render(request, "fontend/index.html", {'info': antony[0], 'education': antony[1]})
 
 
 def show_articles(request):
@@ -59,75 +59,75 @@ def show_articles(request):
         "hl": "th",
         "user": str(scholar_id)
     }
-    articles = scholar.get_articles(params)
+    articles = get_articles(params)
     return render(request, 'fontend/articles.html', {'articles': articles, 'size': len(articles)})
 
 
 def timetable(request):
     email = request.GET['email']
     if email == 'wansureem@nu.ac.th':
-        x = scholar.get_timetable1(data.wansuree[0]['timetable'])
+        x = get_timetable1(wansuree[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'prasartb@nu.ac.th':
-        x = scholar.get_timetable1(data.prasart[0]['timetable'])
+        x = get_timetable1(prasart[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'chakkrits@nu.ac.th':
-        x = scholar.get_timetable1(data.chakkrit[0]['timetable'])
+        x = get_timetable1(chakkrit[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'ekkasitt@nu.ac.th':
-        x = scholar.get_timetable1(data.ekkasit[0]['timetable'])
+        x = get_timetable1(ekkasit[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'nattavadeeho@nu.ac.th':
-        x = scholar.get_timetable1(data.nattavadee[0]['timetable'])
+        x = get_timetable1(nattavadee[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'wuttipongr@nu.ac.th':
-        x = scholar.get_timetable1(data.wuttipong[0]['timetable'])
+        x = get_timetable1(wuttipong[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'janjirap@nu.ac.th':
-        x = scholar.get_timetable1(data.janjira[0]['timetable'])
+        x = get_timetable1(janjira[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'adirekr@nu.ac.th':
-        x = scholar.get_timetable1(data.adirek[0]['timetable'])
+        x = get_timetable1(adirek[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'anongporns@nu.ac.th':
-        x = scholar.get_timetable1(data.anongporn[0]['timetable'])
+        x = get_timetable1(anongporn[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'tawint@nu.ac.th':
-        x = scholar.get_timetable1(data.tawin[0]['timetable'])
+        x = get_timetable1(tawin[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'kreangsakt@nu.ac.th':
-        x = scholar.get_timetable1(data.kreangsak[0]['timetable'])
+        x = get_timetable1(kreangsak[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'jaratsrir@nu.ac.th':
-        x = scholar.get_timetable1(data.jaratsri[0]['timetable'])
+        x = get_timetable1(jaratsri[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'winaiw@nu.ac.th':
-        x = scholar.get_timetable1(data.winai[0]['timetable'])
+        x = get_timetable1(winai[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'kraisakk@nu.ac.th':
-        x = scholar.get_timetable1(data.kraisak[0]['timetable'])
+        x = get_timetable1(kraisak[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'sanyak@nu.ac.th':
-        x = scholar.get_timetable1(data.sanya[0]['timetable'])
+        x = get_timetable1(sanya[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'sutasineec@nu.ac.th':
-        x = scholar.get_timetable1(data.sutasinee[0]['timetable'])
+        x = get_timetable1(sutasinee[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'phisetphongs@nu.ac.th':
-        x = scholar.get_timetable1(data.phisetphong[0]['timetable'])
+        x = get_timetable1(phisetphong[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'duangduenr@nu.ac.th':
-        x = scholar.get_timetable1(data.duangduen[0]['timetable'])
+        x = get_timetable1(duangduen[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'thanathornp@nu.ac.th':
-        x = scholar.get_timetable1(data.thanathorn[0]['timetable'])
+        x = get_timetable1(thanathorn[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'nattaponk@nu.ac.th':
-        x = scholar.get_timetable1(data.nattapon[0]['timetable'])
+        x = get_timetable1(nattapon[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'nattaponk@nu.ac.th':
-        x = scholar.get_timetable1(data.nattapon[0]['timetable'])
+        x = get_timetable1(nattapon[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
     elif email == 'antonyh@nu.ac.th':
-        x = scholar.get_timetable1(data.antony[0]['timetable'])
+        x = get_timetable1(antony[0]['timetable'])
         return render(request, 'fontend/timetable.html', {'table': x})
